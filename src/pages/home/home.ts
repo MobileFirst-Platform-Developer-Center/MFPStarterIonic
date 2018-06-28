0075a67aebc9b4d48d53f0aa91a937f9b91f9ae5
Vittal Pai <vittalpai@ymail.com>
2018-06-28 21:50:10 +0530

    Add token call to MFP Server

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  title: string;
+  status: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,  private zone: NgZone) {
+    this.title = "Hello MobileFirst";
+  }
 
+  pingMFP() {
+    this.zone.run(() => {
+      this.title = "Hello MobileFirst";
+      this.status = "Connecting to Server...";
+    });
+    WLAuthorizationManager.obtainAccessToken("").then(
+      (token) => {
+        console.log('-->  pingMFP(): Success ', token);
+        this.zone.run(() => {
+          this.title = "Yay!";
+          this.status = "Connected to MobileFirst Server";
+        });
+      }, (error) => {
+        console.log('-->  pingMFP(): failure ', error.responseText);
+        this.zone.run(() => {
+         this.title = "Bummer...";
+         this.status = "Failed to connect to MobileFirst Server";
+        });
+      }
+    )
   }
 
 }
